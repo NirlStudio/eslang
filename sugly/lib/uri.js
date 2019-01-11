ed751f0c0dd3f0b2e8de14a989574c46d5f79e11
Leevi <leevi@nirlstudio.com>
2019-01-11 22:42:46 +1300

    add test for lib/uri

--- sugly/lib/uri.js
+++ sugly/lib/uri.js
@@ -3,13 +3,45 @@
 module.exports = function ($void, JS) {
   var $ = $void.$
   var $Object = $.object
+  var link = $void.link
   var $export = $void.export
-  var copyType = $void.copyType
 
-  $export($, 'uri', copyType($Object.empty(), JS, {
-    'encodeURI': 'encode',
-    'decodeURI': 'decode',
-    'encodeURIComponent': 'escape',
-    'decodeURIComponent': 'unescape'
-  }))
+  var uri = $Object.empty()
+  link(uri, 'encode', function (str) {
+    return typeof str !== 'string' ? null : encodeURI(str)
+  })
+
+  link(uri, 'decode', function (str, defaultValue) {
+    if (typeof str !== 'string') {
+      return typeof defaultValue === 'undefined' ? null : defaultValue
+    }
+    if (typeof defaultValue === 'undefined') {
+      return decodeURI(str)
+    }
+    try {
+      return decodeURI(str)
+    } catch (err) {
+      return defaultValue
+    }
+  })
+
+  link(uri, 'escape', function (str) {
+    return typeof str !== 'string' ? null : encodeURIComponent(str)
+  })
+
+  link(uri, 'unescape', function (str, defaultValue) {
+    if (typeof str !== 'string') {
+      return typeof defaultValue === 'undefined' ? null : defaultValue
+    }
+    if (typeof defaultValue === 'undefined') {
+      return decodeURIComponent(str)
+    }
+    try {
+      return decodeURIComponent(str)
+    } catch (err) {
+      return defaultValue
+    }
+  })
+
+  $export($, 'uri', uri)
 }
