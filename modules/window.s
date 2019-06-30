060eca96ac4f143729c4cb7b71776c04055fe517
Leevi <leevi@nirlstudio.com>
2019-06-30 23:57:47 +1200

    add operator bind

--- modules/window.s
+++ modules/window.s
@@ -2,3 +2,9 @@ const window (import "$window");
 
 # only expose necessary member of native window.
 export (document, navigator, location) window;
+
+# bind elements to their ids.
+(export bind (=? ()
+  (for &el in (operation slice operand) (if (&el is-a symbol)
+    (var &el (? document:: getElementById (&el key);
+).
