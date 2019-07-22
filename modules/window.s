ede83f2ed3b1dd299387b6931e02002fd4a9375b
Leevi <leevi@nirlstudio.com>
2019-07-22 19:09:48 +1200

    add event on & off methods in window module;

--- modules/window.s
+++ modules/window.s
@@ -8,3 +8,34 @@ export (document, navigator, location) window;
   (for &el in (operation slice operand) (if (&el is-a symbol)
     (var &el (? document:: getElementById (&el key);
 ).
+
+# if the target is a string, it's indeed the event name.
+(const prepare-by (=> target
+  (target is-a string) ? (@ window, 0), (@ target, 1);
+).
+
+# add an event listener and return it.
+(export on (? (document "addEventListener":: is-a function)
+  (=> (target, event, listener, options)
+    let (target, offset) (prepare-by target);
+    target "addEventListener":: apply target, (arguments slice offset);
+    arguments: (offset + 1);
+  ).
+  (=> (target, event, listener)
+    let (target, offset) (prepare-by target);
+    target attachEvent 'on$(arguments: offset)', listener;
+    arguments: (offset + 1);
+  ).
+).
+
+# remove an event listener and return the target.
+(export off (? (document "removeEventListener":: is-a function)
+  (=> (target, event, listener, options)
+    let (target, offset) (prepare-by target);
+    target "removeEventListener":: apply target, (arguments slice offset); target
+  ).
+  (=> (target, event, listener)
+    let (target, offset) (prepare-by target);
+    target detachEvent 'on$(arguments: offset)', listener; target
+  ).
+).
